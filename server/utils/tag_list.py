8a6dadc3511b7d21cf417d73ddd89adb45e47899
dstoeber18 <dstoeber@emich.edu>
2024-04-08 00:00:29 -0400

    tag_list.py should automatically pull from the db tags table

--- server/utils/tag_list.py
+++ server/utils/tag_list.py
@@ -1,3 +1,5 @@
+from utils.database import get_db
+
 tag_list = [
     "Good",
     "Bad",
@@ -6,3 +8,28 @@ tag_list = [
     "What's wrong with you?",
     "I hate you."
 ]
+
+def get_tag_list():
+    engine = get_db()
+
+    try:
+        #do not created connection here, connection = engine.connect() is used as the connection
+        with engine.connect() as connection:
+            #create a cursor to interact with the database
+            trans = connection.begin()
+
+            #get a list of all tags from db
+            connection.execute("SELECT * FROM tags;")
+            taglist = connection.fetchall()
+
+            return taglist
+
+
+            #handle any errors that occur during the database connection
+    except Exception as error:
+        print("Error:", error)
+
+
+
+
+    
