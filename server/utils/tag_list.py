a48188a5a3451f3bed8e8d5906c07fce8523d635
dstoeber18 <dstoeber@emich.edu>
2024-04-08 18:25:10 -0400

    updated taglist to mirror changes in model list

--- server/utils/tag_list.py
+++ server/utils/tag_list.py
@@ -1,25 +1,18 @@
 from utils.database import get_db
+from sqlalchemy import text
 
 def get_tag_list():
     engine = get_db()
 
     try:
-        #do not created connection here, connection = engine.connect() is used as the connection
         with engine.connect() as connection:
-            #create a cursor to interact with the database
-            cursor = connection.cursor()
-
-            #get a list of all tags from db
-            cursor.execute("SELECT * FROM tags;")
-            taglist = connection.fetchall()
+            result_proxy = connection.execute(text("SELECT tag FROM tags"))
+            taglist = [row[0] for row in result_proxy.fetchall()] 
 
             return taglist
 
-
-            #handle any errors that occur during the database connection
     except Exception as error:
         print("Error:", error)
-
 
 
 
