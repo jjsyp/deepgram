21e3a47b8ec87814049b804612714c2f9abda135
lthacke3 <lthacke3@emich.edu>
2024-04-09 15:50:33 -0400

    I couldn't let my baby out pf development without a pretty login button

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,5 +1,26 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar';
+import { styled } from '@mui/material';
+
+const LoginButton = styled('button')(() => ({
+    padding: '16px',
+    background: '#101014',
+    border: 'none',
+    borderRadius: '16px',
+    fontSize: '16px',
+    color: '#eeeeee',
+    '&:hover': {
+        background: '#4c4c4d',
+        transition: '200ms'
+    },
+    '&:not(hover)': {
+        transition: '200ms'
+    },
+    '&:active': {
+        background: '#21c5a4',
+        color: '#101014'
+    }
+}))
 
 /**
  * HomePage component which contains a welcome message and a Login with Google button.
@@ -23,7 +44,7 @@ const HomePage = () => {
             <h1>DeepGram TTS Benchmarking Tool</h1>
         </div>
         <div className="container">
-            <button onClick={loginHandler}>Login with Google</button>
+            <LoginButton onClick={loginHandler}>Login with Google</LoginButton>
         </div>
     </div>
   </>
